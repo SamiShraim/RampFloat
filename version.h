e66493aa404bbdfbe507d23a8b7cc2467ddddc54
SamiShraim <sami.shraim@brunvoll.no>
2023-01-23 12:07:32 +0100

    push tag

---
+++ version.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#define version_major 0x01
+#define version_minor 0x02
+#define version_patch 0x01
+
+const int version_combined = version_major << 16 | version_minor << 8 | version_patch;
