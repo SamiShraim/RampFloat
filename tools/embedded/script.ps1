ec35e64bb5dd712350957535602e8f5b5643318c
SamiShraim <sami.shraim@brunvoll.no>
2023-01-24 10:31:29 +0100

    passs

--- tools/embedded/script.ps1
+++ tools/embedded/script.ps1
@@ -1,16 +1,12 @@
 param($Paths)
 $include= "$Paths/mps170/apps/version.h"
 
-$gitTags = git tag -l
+$gitTags = "mps179-display-1.2.3 mps179-display-1.3.3 mps179-1.2.3 mps179-2.2.3"
 $tag_name = ($gitTags | Select-String -Pattern 'mps179' -NotMatch 'display' | Sort-Object -Property Version | Select-Object -Last 1).Line
+
 Write-Output $tag_name
 
 Write-Host "the latest tag is $gitTags"
 Write-Host "the Current path is $Paths"
-
-
-
-
-
 
 cat $include
