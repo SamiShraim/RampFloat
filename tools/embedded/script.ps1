bdb9b5a3cfd26fa399991b023951c2d7fe2440a8
SamiShraim <sami.shraim@brunvoll.no>
2023-01-24 11:26:42 +0100

    sdfsdf

--- tools/embedded/script.ps1
+++ tools/embedded/script.ps1
@@ -1,11 +1,7 @@
 param($Paths)
 $include= "$Paths/mps170/apps/version.h"
 
-$gitTags = 'mps179-display-1.2.3', 'mps179-display-1.3.3', 'mps179-1.2.3', 'mps179-2.2.3'
-$tag_name = 'mps179-display-1.3.3', 'mps179-1.2.3' | Select-String -Pattern 'mps179' -NotMatch 'display' | Sort-Object -Property Version | Select-Object -Last 1
-Write-Output $tag_name
-
-$tag_name = ($gitTags | Select-String -Pattern 'mps179' -NotMatch 'display' | Sort-Object -Property Version | Select-Object -Last 1).Line
+$tag_name = (git tag -l | Select-String -Pattern 'mps179'| Select-String -Pattern 'display' -NotMatch | Sort-Object -Property Version | Select-Object -Last 1).Line
 
 Write-Output $tag_name
 
