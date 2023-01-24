286c629908d7a30b150cd7a4f1561c0b4608cf6d
SamiShraim <sami.shraim@brunvoll.no>
2023-01-24 10:26:18 +0100

    asfsdsdf

--- tools/embedded/script.ps1
+++ tools/embedded/script.ps1
@@ -1,8 +1,16 @@
 param($Paths)
+$include= "$Paths/mps170/apps/version.h"
+
 $gitTags = git tag -l
+$tag_name = ($gitTags | Select-String -Pattern 'mps179' -NotMatch 'display' | Sort-Object -Property Version | Select-Object -Last 1).Line
+Write-Output $tag_name
 
 Write-Host "the latest tag is $gitTags"
 Write-Host "the Current path is $Paths"
 
-$include= "$Paths/mps170/apps/version.h"
+
+
+
+
+
 cat $include
