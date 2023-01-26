975a05e0a70a65d58240ea8983fbeea82c2c3623
SamiShraim <sami.shraim@brunvoll.no>
2023-01-26 13:17:43 +0100

    asdsad

--- tools/embedded/script.ps1
+++ tools/embedded/script.ps1
@@ -2,10 +2,7 @@ param($Paths)
 $include= "$Paths/mps170/apps/version.h"
 
 $tag_name = (git tag -l | Select-String -Pattern 'mps179'| Select-String -Pattern 'display' -NotMatch | Sort-Object -Descending | Select-Object -First 1).Line
-Write-Output $tag_name
-
 $current_tag_name = (git describe --tags --abbrev=0 | Select-String -Pattern 'mps179'| Select-String -Pattern 'display' -NotMatch).Line
-Write-Output $current_tag_name
 
 Write-Host "the latest tag is $tag_name"
 Write-Host "the Current commit tag is $current_tag_name"
